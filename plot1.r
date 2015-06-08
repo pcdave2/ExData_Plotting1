c91619f0d354c152242f7c60cb454599266ae32d
Parth <pcdave26@gmail.com>
2015-06-08 13:24:33 -0700

    Add plot1.r.

--- plot1.r
+++ plot1.r
@@ -1,3 +1,5 @@
-data <- read.table("household_power_consumption.txt", header = TRUE, stringsAsFactors=FALSE, sep=";")data.sub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+data <- read.table("household_power_consumption.txt", header = TRUE, stringsAsFactors=FALSE, sep=";")
+data.sub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 hplot <- as.numeric(as.vector(data.sub$Global_active_power))
 hist(hplot, col="Red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+png("plot1.png", width=480, height=480)
