83a565683b1e6290b95a82c983a870ddf0e9d35a
Parth <pcdave26@gmail.com>
2015-06-08 13:27:21 -0700

    Add plot2.r.

--- plot2.r
+++ plot2.r
@@ -2,3 +2,4 @@ data <- read.table("household_power_consumption.txt", header = TRUE, stringsAsFa
 data.sub <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 dT <- as.POSIXlt(paste(as.Date(data.sub$Date, format="%d/%m/%Y"), data.sub$Time, sep=" "))
 plot(dT, data.sub$Global_active_power, xlab=" ", ylab="Global Active Power (kilowatts)", lines(dT, data.sub$Global_active_power, type="l"))
+png("plot2.png", width=480, height=480)
